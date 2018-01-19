76b527061156a0379593131b5e235473f5173613
Amanda Wehrman <arwehrman@gmail.com>
2018-01-19 21:24:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const  logDriversByHometown = function(drivers, location) {
   })
 }
 
-const driversByRevenue = function(drivers) {
-  return drivers.sort(function(driver1, driver2) {
-    return driver1.revenue - driver2.revenue;
-  });
-  }
+const driversByRevenue = function (drivers) {
+    return drivers.slice().sort(function (driver1, driver2) {
+        return driver1.revenue - driver2.revenue;
+    });
+};
